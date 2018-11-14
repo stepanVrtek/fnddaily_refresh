65095b97e36738d368da82fb3babefff4c174c6c
stepanVrtek <stepan.vrtek@gmail.com>
2018-11-14 17:13:14 +0100

    reqs added

--- process.py
+++ process.py
@@ -12,7 +12,7 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', sco
 client = gspread.authorize(creds)
 d = date.today()
 
-source = client.open("Zdroj (kopie).xlsm").sheet1
+source = client.open("_source.xls").sheet1
 fnd = client.open('fnddaily{}.xls'.format(d.strftime('%d%m%Y'))).sheet1
 
 source_df = pd.DataFrame(source.get_all_records())
@@ -25,8 +25,8 @@ still_not_existing = True
 # add not existing dates columns
 while still_not_existing:
     if d.strftime('%-d.%-m.%Y') not in existing_columns:
-        source.update_cell(1, existing_columns.__len__()+1, d.strftime('%-d.%-m.%Y'))
-        source = client.open("Zdroj (kopie).xlsm").sheet1
+        source.update_cell(1, len(existing_columns) + 1, d.strftime('%-d.%-m.%Y'))
+        source = client.open("_source.xls").sheet1
         source_df = pd.DataFrame(source.get_all_records())
         existing_columns = list(source_df.columns.values)
     else:
@@ -38,10 +38,19 @@ for index, row in fnddaily.iterrows():
     try:
         # column index
         column_index_0 = source_df.columns.get_loc(row['Datum'])
-        column_index = existing_columns.__len__() - 1 - column_index_0
+        column_index = len(existing_columns) - 1 - column_index_0
 
         # row index
-        row_index = source_df.index[source_df['ISIN'] == row['ISIN']].tolist()[0]
-        source.update_cell(row_index+2, column_index+1, row['Cena/ks'])
+        row_index = source_df.index[source_df['ISIN'] == row['ISIN']].tolist()
+        if len(row_index) > 0:
+            ri = row_index[0]
+            source.update_cell(ri+2, column_index+1, row['Cena/ks'])
+        else:
+            continue
+
+        for i in range(column_index):
+            if source
     except:
         pass
+
+# FILL MISSING VALUES FOR PAST DATES (AS LAST KNOWN PAST DAY VALUE)
